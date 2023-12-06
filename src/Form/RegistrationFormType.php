1db06627d59527fa0127fc22861dc289be9f789f
BarnoMamadzhanova <b@mamadzhanova.com>
2023-12-06 12:21:59 +0100

    creating login and register

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('first_name', null, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "You can't leave this field empty"
+                    ]),
+                    new Length(['min' => 3, 'minMessage' => "This field can't be less then 3 characters"]),
+                ],
+            ])
+            ->add('last_name', null, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "You can't leave this field empty"
+                    ]),
+                    new Length(['min' => 3, 'minMessage' => "This field can't be less then 3 characters"]),
+                ],
+            ])
+            ->add('email')
+            ->add('agreeTerms', CheckboxType::class, [
+                                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
+            ->add('photo', FileType::class, [
+                'label' => 'Your photo',
+                'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px'],
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => true,
+
+                // unmapped fields can't define their validation using attributes
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                           'image/png',
+                           'image/jpeg',
+                           'image/jpg',
+                       ],
+                       'mimeTypesMessage' => 'Please upload a valid image file',
+                    ])
+                ],
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 8,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
